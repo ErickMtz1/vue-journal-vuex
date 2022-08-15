344413ff444e26310f29a0251391ff053acbd4cd
Erick Martínez <erick.salas.mtz@hotmail.com>
2022-08-15 13:37:53 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+    {
+        id: new Date().getTime(),
+        date: new Date().toDateString(),
+        text: 'Laborum nostrud amet excepteur duis. Duis nisi magna dolore enim culpa sunt sit consectetur mollit sint consequat. Cupidatat qui veniam laboris ut. Sit sint adipisicing ullamco exercitation eu et non velit nisi commodo dolore pariatur ex. Amet nisi non officia non cillum aliqua nisi. Consectetur quis sunt id consequat fugiat laborum. Exercitation culpa veniam aute incididunt excepteur ex nisi et nisi exercitation.',
+        picture: null, 
+    },
+    {
+        id: new Date().getTime() +1000,
+        date: new Date().toDateString(),
+        text: 'Anim qui duis consequat culpa cupidatat et mollit minim. Officia commodo ullamco mollit sint duis sunt proident qui Lorem occaecat nostrud. Minim deserunt minim amet officia sint anim est ut. Ullamco elit enim officia nisi sint nulla commodo eiusmod magna commodo est.',
+        picture: null, 
+    },
+    {
+        id: new Date().getTime() +2000,
+        date: new Date().toDateString(),
+        text: 'Sit exercitation dolor do aliquip magna sit occaecat. Deserunt cupidatat cupidatat ad aliquip aute est exercitation tempor duis laboris aliquip. Eu elit aliqua mollit sint quis exercitation.',
+        picture: null, 
+    },
+],
+})
